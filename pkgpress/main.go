3ce75819a306fce71cb1aeec29d2c1ad015be1c5
Douglas Fils <drfils@gmail.com>
2018-03-18 09:57:59 -0500

    syncing up lots of file admin...

---
+++ pkgpress/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"oceanleadership.org/frictionless/THROUGHPUTDataPackages/pkgpress/lphandler"
+)
+
+// TODO:
+// How will schemaFromPackage deal with data in an S3 system?
+// How do I materialize the datapackage?
+
+// MyServer struct for mux router
+type MyServer struct {
+	r *mux.Router
+}
+
+func main() {
+	log.Println("Make a landing page for a data package")
+
+	searchroute := mux.NewRouter()
+	searchroute.HandleFunc("/lptest", lphandler.PresentPackage) // the REAL handler for this URL
+	http.Handle("/lptest", searchroute)
+
+	ar := mux.NewRouter()
+	ar.PathPrefix("/assets/").Handler(http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets"))))
+	http.Handle("/assets/", &MyServer{ar})
+
+	log.Printf("About to listen on 9900. Go to http://127.0.0.1:9900/")
+
+	err := http.ListenAndServe(":9900", nil)
+	// http 2.0 http.ListenAndServeTLS(":443", "server.crt", "server.key", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func (s *MyServer) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	rw.Header().Set("Access-Control-Allow-Origin", "*")
+	rw.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	rw.Header().Set("Access-Control-Allow-Headers",
+		"Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+
+	// Let the Gorilla work
+	s.r.ServeHTTP(rw, req)
+}
+
+func addDefaultHeaders(fn http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		fn(w, r)
+	}
+}
---
+++ pkgpress/osproxy/osproxy.go
@@ -0,0 +1,39 @@
+package osproxy
+
+import (
+	"log"
+
+	"github.com/frictionlessdata/datapackage-go/datapackage"
+)
+
+func GetObjectByURL(urlloc string) {
+	log.Printf("Getting file at URL %s", urlloc)
+
+	// steps
+	// parse URL to get the ID needed to get the file
+	// this will be the sha value of the package zip
+
+	// read from S3
+	// pass file object back to
+
+}
+
+// SchemaFromPackage Loads the data package descriptor from the specified URL or file path.
+// If path has the ".zip" extension, it will be saved in local filesystem and decompressed before loading.
+// Load zip via URL call into S3??
+func SchemaFromPackage() string {
+	pkg, err := datapackage.Load("../CarpLake/datapackage.json")
+	if err != nil {
+		log.Println("error in loading data package json")
+	}
+
+	// The following reads the tabular data OK
+	resource := pkg.GetResource("schemaorg") // could I also read the "schemaorg" JSON file in and use it?
+	contents, err := resource.RawRead()
+	if err != nil {
+		log.Printf("Error getting schema.org entry: %v\n", err)
+		// return nil, err
+	}
+
+	return string(contents) // just grab the first "thing" and return it..  we are just testing pipelines...
+}
