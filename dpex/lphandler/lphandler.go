ea165442c281daccdb7c2c28b264868e99b0bf91
Douglas Fils <drfils@gmail.com>
2018-05-29 15:09:44 -0500

    package updates...

--- dpex/lphandler/lphandler.go
+++ dpex/lphandler/lphandler.go
@@ -1,16 +1,17 @@
 package lphandler
 
 import (
+	"io/ioutil"
 	"log"
 	"net/http"
 
 	"github.com/alecthomas/template"
-	"oceanleadership.org/frictionless/THROUGHPUTDataPackages/dpex/outfitter"
+	"github.com/frictionlessdata/datapackage-go/datapackage"
 )
 
 // PresentPackage is the handler for the landing page action
 func PresentPackage(w http.ResponseWriter, r *http.Request) {
-	so := outfitter.SchemaFromPackage()
+	so := schemaFromPackage()
 	templateFile := "./templates/landingPage.html"
 
 	ht, err := template.New("Template").ParseFiles(templateFile) //open and parse a template text file
@@ -22,4 +23,23 @@ func PresentPackage(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Printf("Template execution failed: %s", err)
 	}
+}
+
+func schemaFromPackage() string {
+	pkg, err := datapackage.Load("../datapackages/simple-geojson/datapackage.json")
+	if err != nil {
+		log.Printf("Error getting package: %v\n", err)
+		return "Error getting package"
+	}
+
+	resource := pkg.GetResource("example") // could I also read the "schemaorg" JSON file in and use it?
+	rc, err := resource.RawRead()
+	defer rc.Close()
+	contents, _ := ioutil.ReadAll(rc)
+	if err != nil {
+		log.Printf("Error getting raw read of entry: %v\n", err)
+		return "Error getting schema.org entry"
+	}
+
+	return string(contents) // just grab the first "thing" and return it..  we are just testing pipelines...
 }
